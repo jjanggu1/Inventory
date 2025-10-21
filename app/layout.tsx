74557fc7d0198c97a4548ba7b326de2151208171
jjanggu1 <sdj970514@naver.com>
2025-10-21 19:45:51 +0900

    feat: Header 컴포넌트 퍼블리싱 완료

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "@/widgets/Header/Header";
 
 export const metadata: Metadata = {
   title: "Inventory For 사장님",
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased ${pretendard.variable} font-pretendard`}>
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/widgets/Header/Header.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { FiMenu, FiMoreVertical } from "react-icons/fi";
+
+export default function Header() {
+  return (
+    <header className="w-full flex items-center justify-between px-6 py-4 text-label-500 bg-label-800">
+      <div>
+        <button className="p-4 rounded-md cursor-pointer active:scale-95 active:duration-100 transition-all">
+          <FiMenu className="text-2xl" />
+        </button>
+      </div>
+      <nav>
+        <ul className="flex items-center gap-4 text-title-3 font-bold">
+          <li className="px-8 py-4 rounded-md cursor-pointer text-label-100 active:bg-label-700 active:scale-95 active:duration-100 transition-all">
+            <Link href="/">테이블</Link>
+          </li>
+          <li className="px-8 py-4 rounded-md cursor-pointer active:bg-label-700 active:scale-95 active:duration-100 transition-all">
+            <Link href="/">현황</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex items-center gap-2">
+        <div className="text-body-3 font-semibold text-label-200">
+          <p>10.21 (월)</p>
+          <p>오후 6:23</p>
+        </div>
+        <div>
+          <button className="p-4 mx-4 rounded-md cursor-pointer active:scale-95 active:duration-100 transition-all">
+            <FiMoreVertical className="text-2xl" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
