f4e5ed376a7b9678536351e0436ce08690cfd1ab
jjanggu1 <sdj970514@naver.com>
2025-10-21 16:12:42 +0900

    refactor: 폴더 구조 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 };
 
 const pretendard = localFont({
-  src: "../../public/fonts/PretendardVariable.woff2",
+  src: "../public/fonts/PretendardVariable.woff2",
   variable: "--font-pretendard",
   display: "swap",
   weight: "45 920",
