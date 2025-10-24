b1ca0ce1995a53686b971b848b1f0ef9835bd673
jjanggu1 <sdj970514@naver.com>
2025-10-24 15:49:05 +0900

    feat: 현재시간 데이터 UI에 바인딩 완료

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import Link from "next/link";
 import { FiMenu, FiMoreVertical } from "react-icons/fi";
+import { useCurrentDateTime } from "@/shared/hooks/useCurrentDateTime";
 
 export default function Header() {
+  const currentDateTime = useCurrentDateTime();
   return (
     <header className="w-full flex items-center justify-between px-6 py-4 text-label-500 bg-label-800">
       <div>
@@ -21,8 +25,14 @@ export default function Header() {
       </nav>
       <div className="flex items-center gap-2">
         <div className="text-body-3 font-semibold text-label-200">
-          <p>10.21 (월)</p>
-          <p>오후 6:23</p>
+          <p>
+            {currentDateTime.month}.{currentDateTime.day} (
+            {currentDateTime.dayOfWeek})
+          </p>
+          <p>
+            {currentDateTime.ampm} {currentDateTime.hours}:
+            {currentDateTime.minutes}
+          </p>
         </div>
         <div>
           <button className="p-4 mx-4 rounded-md cursor-pointer active:scale-95 active:duration-100 transition-all">
