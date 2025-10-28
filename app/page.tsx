894fe5117cfd7b7ee5efe9d7af3dad016e170f33
jjanggu1 <sdj970514@naver.com>
2025-10-28 15:41:52 +0900

    feat: useMenuStore 상태에 따른 메뉴UI 노출 조건 생성 및 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import Menu from "@/shared/ui/menu";
+import { useMenuStore } from "@/shared/store";
+
 export default function Home() {
+  const { isMenuOpen } = useMenuStore();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <main className="flex-1 relative px-6 items-center justify-items-center">
       <h1 className="text-label-100 text-display-1">Inventory For 사장님</h1>
-    </div>
+      {isMenuOpen && <Menu />}
+    </main>
   );
 }
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,20 +1,30 @@
 "use client";
 
 import Link from "next/link";
-import { FiMenu, FiMoreVertical } from "react-icons/fi";
+import { FiMenu, FiMoreVertical, FiX } from "react-icons/fi";
 import { useCurrentDateTime } from "@/shared/hooks/useCurrentDateTime";
+import { useMenuStore } from "@/shared/store";
 
 export default function Header() {
   const currentDateTime = useCurrentDateTime();
+  const { isMenuOpen, setIsMenuOpen } = useMenuStore();
+
   return (
     <header className="w-full flex items-center justify-between px-6 py-4 text-label-500 bg-label-800">
       <div>
         <button className="p-4 rounded-md cursor-pointer active:scale-95 active:duration-100 transition-all">
-          <FiMenu className="text-2xl" />
+          {isMenuOpen ? (
+            <FiX
+              className="text-2xl text-label-100"
+              onClick={() => setIsMenuOpen(false)}
+            />
+          ) : (
+            <FiMenu className="text-2xl" onClick={() => setIsMenuOpen(true)} />
+          )}
         </button>
       </div>
       <nav>
-        <ul className="flex items-center gap-4 text-title-3 font-bold">
+        <ul className="flex items-center gap-4 text-body-2 md:text-title-2 font-bold">
           <li className="px-8 py-4 rounded-md cursor-pointer text-label-100 active:bg-label-700 active:scale-95 active:duration-100 transition-all">
             <Link href="/">테이블</Link>
           </li>
