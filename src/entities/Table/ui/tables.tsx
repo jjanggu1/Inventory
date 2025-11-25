bef4cd3e6c8cb7fe1b4b44296276914ed29fedc0
jjanggu1 <sdj970514@naver.com>
2025-11-25 15:29:49 +0900

    feat: 테이블 클릭시 해당 테이블(id)의 주문 페이지로 이동하도록 설정

--- src/entities/Table/ui/tables.tsx
+++ src/entities/Table/ui/tables.tsx
@@ -1,18 +1,22 @@
 "use client";
 
+import Link from "next/link";
+
 export default function Tables() {
   return (
     <>
       {/* 사용가능한 테이블 */}
       {Array.from({ length: 21 }).map((_, index) => (
-        <div
-          key={index}
-          className="w-full lg:min-w-[180px] lg:max-w-[180px] xl:min-w-[260px] xl:max-w-[260px] xl:min-h-[140px] min-h-[100px] lg:max-h-[110px]  bg-background-default rounded-sm border-3 border-secondary-600"
-        >
-          <h1 className="text-body-3 text-label-900 font-semibold text-center pt-2">
-            홀1 테이블
-          </h1>
-        </div>
+        <Link href={`/orders/${index}`}>
+          <div
+            key={index}
+            className="w-full lg:min-w-[180px] lg:max-w-[180px] xl:min-w-[260px] xl:max-w-[260px] xl:min-h-[140px] min-h-[100px] lg:max-h-[110px]  bg-background-default rounded-sm border-3 border-secondary-600"
+          >
+            <h1 className="text-body-3 text-label-900 font-semibold text-center pt-2">
+              홀1 테이블
+            </h1>
+          </div>
+        </Link>
       ))}
       {/* 사용중인 테이블 */}
       {Array.from({ length: 3 }).map((_, index) => (
